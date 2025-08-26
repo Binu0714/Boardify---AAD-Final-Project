d4e310451e9ea90b349c4994c4f3e9044be21e1c
Binu0714 <binuj714@gmail.com>
2025-08-26 23:24:59 +0530

    create user entity

--- BackEnd/Bordify-BackEnd/src/main/java/org/example/bordifybackend/BordifyBackEndApplication.java
+++ BackEnd/Bordify-BackEnd/src/main/java/org/example/bordifybackend/BordifyBackEndApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BordifyBackEndApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(BordifyBackEndApplication.class, args);
 	}
 
---
+++ BackEnd/Bordify-BackEnd/src/main/java/org/example/bordifybackend/entity/Role.java
@@ -0,0 +1,5 @@
+package org.example.bordifybackend.entity;
+
+public enum Role {
+    ADMIN, USER
+}

ed6c4e7c27179aaa68c6038418a16306564d923b
Binu0714 <binuj714@gmail.com>
2025-08-26 17:01:49 +0530

    first commit

---
+++ BackEnd/Bordify-BackEnd/src/main/java/org/example/bordifybackend/BordifyBackEndApplication.java
@@ -0,0 +1,13 @@
+package org.example.bordifybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BordifyBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BordifyBackEndApplication.class, args);
+	}
+
+}
---
+++ BackEnd/Bordify-BackEnd/src/test/java/org/example/bordifybackend/BordifyBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bordifybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BordifyBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

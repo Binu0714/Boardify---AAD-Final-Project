162226ae05f2e0a94eebaa7f5d3f4253a8ca156e
Binu0714 <binuj714@gmail.com>
2025-08-26 23:13:46 +0530

    create database and file structure

--- BackEnd/Bordify-BackEnd/src/main/java/org/example/bordifybackend/BordifyBackEndApplication.java
+++ BackEnd/Bordify-BackEnd/src/main/java/org/example/bordifybackend/BordifyBackEndApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BordifyBackEndApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(BordifyBackEndApplication.class, args);
 	}
 

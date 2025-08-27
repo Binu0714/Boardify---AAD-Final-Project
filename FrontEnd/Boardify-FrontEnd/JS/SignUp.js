b6c3cd572ca1f965929828bc224741da59165366
Binu0714 <binuj714@gmail.com>
2025-08-27 20:57:59 +0530

    user registration - frontEnd

---
+++ FrontEnd/Boardify-FrontEnd/JS/SignUp.js
@@ -0,0 +1,40 @@
+$("#signup-form").submit(function (event) {
+    event.preventDefault();
+
+    let username = $("#name").val();
+    let email = $("#email").val();
+    let password = $("#password").val();
+
+    let user = {
+        username: username,
+        password: password,
+        email: email,
+        role: "USER"
+    };
+
+    $.ajax({
+        url: "http://localhost:8080/user/register",
+        method: "POST",
+        contentType: "application/json",
+        data: JSON.stringify(user),
+
+        success: function (response) {
+            Swal.fire({
+                icon: 'success',
+                title: 'Registration Successful',
+                text: 'Redirecting to login page...',
+                confirmButtonText: 'OK'
+            }).then(() => {
+                window.location.href = "LogIn.html";
+            });
+        },
+        error: function () {
+            Swal.fire({
+                icon: 'error',
+                title: 'Registration Failed',
+                text: 'Please try again later.',
+                confirmButtonText: 'Retry'
+            });
+        }
+    });
+});

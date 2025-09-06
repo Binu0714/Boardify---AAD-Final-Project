b87331fe72c82dcb6108d3a2b0f9e6df4a9669c9
Binu0714 <binuj714@gmail.com>
2025-09-07 01:48:57 +0530

    create propertyDTO

---
+++ BackEnd/Bordify-BackEnd/src/main/java/org/example/bordifybackend/Dto/PropertyDTO.java
@@ -0,0 +1,29 @@
+package org.example.bordifybackend.Dto;
+
+import lombok.Data;
+import org.example.bordifybackend.entity.ListedFor;
+import org.example.bordifybackend.entity.PropertyType;
+
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+public class PropertyDTO {
+    private String title;
+    private String description;
+    private PropertyType propertyType;
+    private ListedFor listedFor;
+    private int noOfBeds;
+    private int noOfBaths;
+    private BigDecimal price;
+
+    private String city;
+    private String district;
+    private String address;
+    private BigDecimal latitude;
+    private BigDecimal longitude;
+
+    //front end eken ewanne amenities set ekk.......
+    private Set<Long> amenityIds = new HashSet<>();
+}

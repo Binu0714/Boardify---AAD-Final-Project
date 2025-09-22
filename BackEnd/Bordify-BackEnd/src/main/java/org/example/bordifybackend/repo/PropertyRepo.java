025cf5673b1498fe34120e24409b3cd10c4203a1
Binu0714 <binuj714@gmail.com>
2025-09-22 20:39:14 +0530

    BackEnd - Final Commit

--- BackEnd/Bordify-BackEnd/src/main/java/org/example/bordifybackend/repo/PropertyRepo.java
+++ BackEnd/Bordify-BackEnd/src/main/java/org/example/bordifybackend/repo/PropertyRepo.java
@@ -26,7 +26,6 @@ public interface PropertyRepo extends JpaRepository<Property, Long> {
     @Query("SELECT DISTINCT p.nearestCampus FROM Property p ORDER BY p.nearestCampus ASC")
     List<String> findDistinctUniversities();
 
-
     long countByAvailabilityFalse();
 
     long countByAvailabilityTrue();
